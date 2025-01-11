8cfcff4db2c9bc464adf1fd996e1248804e249a1
Kayathri-maths <kayualagarsamy@gmail.com>
2025-01-11 17:40:40 +0530

    child_profile

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,13 +1,9 @@
-const express= require('express');
+const express = require("express");
 const router = express.Router();
-const userController = require('../controllers/userController')
+const userController = require("../controllers/userController");
 
+router.post("/register", userController.signUp);
+router.post("/login", userController.login);
+router.post("/new-child", userController.newChild);
 
-router.post('/register', userController.signUp)
-router.post('/login',userController.login)
-router.post('/new-child', userController.newChild)
-
-
-
-
-module.exports = router
+module.exports = router;
